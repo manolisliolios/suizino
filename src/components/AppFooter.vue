98f912d8f4c79dfff9e2c0d05e5ca59cba72eeac
Manolis Liolios <emmanouil.liolios@gmail.com>
2022-11-04 14:17:30 +0200

    add git repo link to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,10 +3,14 @@
 </script>
 <template>
 
-  <footer id="footer" class="text-center pt-12">
+  <footer id="footer" class="text-center">
 
     <p>
       Copyright &copy; {{new Date().getFullYear()}}. Created by <a href="https://github.com/manolisliolios" target="_blank" rel="nofollow">Manolis Liolios</a>
+    </p>
+
+    <p class="text-xs mt-2 pb-6">
+      Code is open-source and can be found here: <a href="https://github.com/manolisliolios/suizino" target="_blank" rel="nofollow">https://github.com/manolisliolios/suizino</a>
     </p>
 
   </footer>
