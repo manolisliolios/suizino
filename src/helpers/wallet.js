94cb072d11308233dfc01c718553f4dd39cb5dda
Manolis Liolios <emmanouil.liolios@gmail.com>
2022-11-08 01:35:04 +0200

    fix wrong wallet links (reversed)

--- src/helpers/wallet.js
+++ src/helpers/wallet.js
@@ -12,13 +12,13 @@ const walletProviders = {
         key: 'suiWallet',
         title: 'Sui Wallet',
         logo: logo,
-        url: 'https://chrome.google.com/webstore/detail/ethos-sui-wallet/mcbigmjiafegjnnogedioegffbooigli'
+        url: 'https://chrome.google.com/webstore/detail/sui-wallet/opcgpfmipidbgpenhmajoajpbobppdil'
     },
     ethosWallet: {
         key: 'ethosWallet',
         title: 'Ethos Wallet',
         logo: ethos_logo,
-        url: 'https://chrome.google.com/webstore/detail/sui-wallet/opcgpfmipidbgpenhmajoajpbobppdil'
+        url: 'https://chrome.google.com/webstore/detail/ethos-sui-wallet/mcbigmjiafegjnnogedioegffbooigli'
     }
 }
 
